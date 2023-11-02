2d90abf822311c1bce5f3def8cc0a02eb4bb5d51
MatrixCY3859 <149415854+MatrixCY3859@users.noreply.github.com>
2023-11-02 14:58:54 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -41,11 +41,11 @@ const CONFIG = {
   // 用户技能图标
   GROUP_ICONS: [
     {
-      title_1: 'Nexitally',
-      img_1: 'https://img.nex18.top/APP icons/nex.jpeg',
+      title_1: 'AfterEffect',
+      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
       color_1: '#989bf8',
-      title_2: 'TAG',
-      img_2: 'https://img.nex18.top/APP icons/tag.jpeg',
+      title_2: 'Sketch',
+      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
       color_2: '#ffffff'
     },
     {
