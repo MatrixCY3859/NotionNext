92cf80df9af9e2b568cb5e30937820d089de6342
TechWave-JC <149415854+TechWave-JC@users.noreply.github.com>
2023-12-04 10:59:02 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,25 +5,25 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://onekoko.top' },
+    { title: '欢迎收藏分享本站', url: 'https://onekoko.top' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '收集记录机场信',
+  HEO_HERO_TITLE_2: '节点测速，流媒体解锁',
+  HEO_HERO_TITLE_3: '机场信息收录',
+  HEO_HERO_TITLE_4: '欢迎收藏分享本站',
+  HEO_HERO_TITLE_5: '机场信息收录总览',
+  HEO_HERO_TITLE_LINK: 'https://t.me/TechWave_QZ',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '特别推荐', url: '/category/特别推荐' },
+  HEO_HERO_CATEGORY_2: { title: '实用教程', url: '/category/实用教程' },
+  HEO_HERO_CATEGORY_3: { title: '资源分享', url: '/category/资源分享' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '推荐',
+  HEO_HERO_RECOMMEND_POST_TAG: '特别推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
@@ -37,64 +37,64 @@ const CONFIG = {
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://t.me/TechWave_QZ', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
     {
-      title_1: 'AfterEffect',
-      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
+      title_1: 'Nexitally',
+      img_1: 'https://r2.nex18.top/APP icons/nex.jpeg',
       color_1: '#989bf8',
-      title_2: 'Sketch',
-      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
+      title_2: 'TAG',
+      img_2: 'https://r2.nex18.top/APP icons/tag.jpeg',
       color_2: '#ffffff'
     },
     {
-      title_1: 'Docker',
-      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
+      title_1: '花云',
+      img_1: 'https://r2.nex18.top/APP icons/huacloud.jpeg',
       color_1: '#57b6e6',
-      title_2: 'Photoshop',
-      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
+      title_2: 'AMY',
+      img_2: 'https://r2.nex18.top/JC icons/AMY.png',
       color_2: '#4082c3'
     },
     {
-      title_1: 'FinalCutPro',
-      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
+      title_1: 'Cream',
+      img_1: 'https://r2.nex18.top/JC icons/Cream.png',
       color_1: '#ffffff',
-      title_2: 'Python',
-      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
+      title_2: 'Flybird',
+      img_2: 'https://r2.nex18.top/JC icons/Flybird.png',
       color_2: '#ffffff'
     },
     {
-      title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
+      title_1: 'IMM',
+      img_1: 'https://r2.nex18.top/JC icons/IMM.png',
       color_1: '#eb6840',
-      title_2: 'Principle',
-      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
+      title_2: 'Kuromis',
+      img_2: 'https://r2.nex18.top/JC icons/Kuromis.png',
       color_2: '#8f55ba'
     },
     {
-      title_1: 'illustrator',
-      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
+      title_1: 'MESL',
+      img_1: 'https://r2.nex18.top/JC icons/MESL.png',
       color_1: '#f29e39',
-      title_2: 'CSS3',
-      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
+      title_2: 'Meiying',
+      img_2: 'https://r2.nex18.top/JC icons/Meiying1.png',
       color_2: '#2c51db'
     },
     {
-      title_1: 'JS',
-      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
+      title_1: 'Paofu',
+      img_1: 'https://r2.nex18.top/JC icons/Paofu.png',
       color_1: '#f7cb4f',
-      title_2: 'HTML',
-      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
+      title_2: 'Titan',
+      img_2: 'https://r2.nex18.top/JC icons/Titan.png',
       color_2: '#e9572b'
     },
     {
-      title_1: 'Git',
-      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
+      title_1: 'xfss',
+      img_1: 'https://r2.nex18.top/JC icons/xfss.png',
       color_1: '#df5b40',
-      title_2: 'Rhino',
-      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
+      title_2: 'Xsus',
+      img_2: 'https://r2.nex18.top/JC icons/Xsus.png',
       color_2: '#1f1f1f'
     }
   ],
